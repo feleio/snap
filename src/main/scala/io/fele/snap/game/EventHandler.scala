8f87471e4adf6fb111aa4249a29b8febd3f7b93d
feleio <lokling@gmail.com>
2020-04-29 20:54:16 +0100

    add EventLogger

--- src/main/scala/io/fele/snap/game/EventHandler.scala
+++ src/main/scala/io/fele/snap/game/EventHandler.scala
@@ -7,3 +7,23 @@ abstract class EventHandler {
 class DummyEventHandler extends EventHandler {
   override def dispatch(event: GameEvent): Unit = ()
 }
+
+class EventLogger extends EventHandler {
+  override def dispatch(event: GameEvent): Unit = event match {
+    case GameStarted(firstPlayer) =>
+      log(s"Game Started, the first player is ${firstPlayer.name}.")
+    case PlaceCard(player, card) =>
+      log(s"${player.name}:  $card")
+    case Snap(player, card, lastCard, wonCardsNum) =>
+      log(s"${player.name}:  Snap!")
+      log(s"$card matches $lastCard. ${player.name} won $wonCardsNum cards.")
+    case GameFinishedWithWinner(winner, loser, pile) =>
+      log(s"${winner.name} won the game with ${winner.wonCardsCount} cards, " +
+        s"while ${loser.name} has ${loser.wonCardsCount} cards. " +
+        s"There are still ${pile.size} cards on the pile.")
+    case GameFinishedWithDrawGame =>
+      log(s"Draw game.")
+  }
+
+  private def log(s: String): Unit = println(s)
+}
--- src/test/scala/io/fele/snap/game/GameTest.scala
+++ src/test/scala/io/fele/snap/game/GameTest.scala
@@ -37,7 +37,6 @@ class GameTest extends AnyFreeSpec with Matchers{
         }
       }
       println(statistic)
-
     }
   }
 }
