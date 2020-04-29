27234fc8f0023042a76ac07fe52b1bf5bc39e692
feleio <lokling@gmail.com>
2020-04-29 21:01:12 +0100

    play again loop

--- src/main/scala/io/fele/snap/game/Main.scala
+++ src/main/scala/io/fele/snap/game/Main.scala
@@ -1,36 +1,46 @@
 package io.fele.snap.game
 
-import io.fele.snap.game.MatchingRule._
-import io.fele.snap.model.CardSuit._
-
-
 import scala.io.StdIn.readLine
 import scala.util.{Success, Try}
 
+import io.fele.snap.game.MatchingRule._
+
 
 object Main extends App {
-  var numberOfDecks: Try[Int] = _
-  var matchRuleOption: Try[Int] = _
-
+  var yn = ""
   do {
-    println("How many decks do you want to play with? Please enter a positive integer:")
-    numberOfDecks = Try(readLine().toInt)
-  } while(numberOfDecks.isFailure || numberOfDecks.get <= 0)
-  do {
-    println("How cards should be matched?: 1)on suit 2)on value 3) both Please enter 1, 2 or 3:")
-    matchRuleOption = Try(readLine().toInt)
-  } while(matchRuleOption.isFailure || matchRuleOption.get <= 0 || matchRuleOption.get > 3)
+    val eventHandler: EventHandler = new EventLogger
+    var numberOfDecks: Try[Int] = Try(0)
+    var matchRuleOption: Try[Int] = Try(0)
 
-  val matchRule: MatchingRule = matchRuleOption match {
-    case Success(1) => OnSuit
-    case Success(2) => OnValue
-    case Success(3) => Both
-  }
+    do {
+      println("How many decks do you want to play with? Please enter a positive integer:")
+      numberOfDecks = Try(readLine().toInt)
+    } while(numberOfDecks.isFailure || numberOfDecks.get <= 0)
+    do {
+      println("How cards should be matched?:   1)on suit   2)on value   3) both?    Please enter 1, 2 or 3:")
+      matchRuleOption = Try(readLine().toInt)
+    } while(matchRuleOption.isFailure || matchRuleOption.get <= 0 || matchRuleOption.get > 3)
 
-  val config = Config(
-    player0Name = "Mike",
-    player1Name = "Chun",
-    numberOfDecks = numberOfDecks.get,
-    matchingRule = matchRule,
-  )
+    val matchRule: MatchingRule = matchRuleOption match {
+      case Success(1) => OnSuit
+      case Success(2) => OnValue
+      case Success(3) => Both
+    }
+
+    val config = Config(
+      player0Name = "Mike",
+      player1Name = "Chun",
+      numberOfDecks = numberOfDecks.get,
+      matchingRule = matchRule,
+    )
+    val game = new Game(config, eventHandler)
+    game.start()
+
+    do {
+      println("Play again? Please enter yes or no:")
+      yn = readLine()
+    } while(yn != "yes" && yn != "no")
+  } while (yn == "yes")
+  println("Bye!")
 }
