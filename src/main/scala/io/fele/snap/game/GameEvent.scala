3c82b5ebdb2d053803c5b1c87994cdf6c50b441c
feleio <lokling@gmail.com>
2020-04-29 20:35:10 +0100

    add game result

--- src/main/scala/io/fele/snap/game/GameEvent.scala
+++ src/main/scala/io/fele/snap/game/GameEvent.scala
@@ -3,9 +3,10 @@ package io.fele.snap.game
 import io.fele.snap.model.Card
 
 sealed trait GameEvent
+sealed trait GameResult extends GameEvent
 
 case class GameStarted(firstPlayer: Player) extends GameEvent
 case class PlaceCard(player: Player, card: Card) extends GameEvent
 case class Snap(player: Player, card: Card, lastCard: Card, wonCardsNum: Int) extends GameEvent
-case class GameFinishedWithWinner(winner: Player, loser: Player, pile: List[Card]) extends GameEvent
-case object GameFinishedWithDrawGame extends GameEvent
+case class GameFinishedWithWinner(winner: Player, loser: Player, pile: List[Card]) extends GameResult
+case object GameFinishedWithDrawGame extends GameResult
