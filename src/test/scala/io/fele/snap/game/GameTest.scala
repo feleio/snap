36888ae4ba20715c6b000b48464a5c5306fe1e1a
feleio <lokling@gmail.com>
2020-04-29 20:53:04 +0100

    fairness test

--- src/test/scala/io/fele/snap/game/GameTest.scala
+++ src/test/scala/io/fele/snap/game/GameTest.scala
@@ -8,12 +8,12 @@ import org.scalatest.matchers.should.Matchers
 class GameTest extends AnyFreeSpec with Matchers{
   val player0Name = "A"
   val player1Name = "B"
-  val numberOfDecks = 3
+  val numberOfDecks = 10
   val matchingRule: MatchingRule = Both
+  val config = Config(player0Name, player1Name, numberOfDecks, matchingRule)
 
   "Game" - {
     "constructor should initialize Game correctly" in {
-      val config = Config(player0Name, player1Name, numberOfDecks, matchingRule)
       val game: Game = new Game(config)
       game.config should be(config)
       game.allCards.size should be (Deck.cards.size * numberOfDecks)
@@ -21,6 +21,23 @@ class GameTest extends AnyFreeSpec with Matchers{
       game.players(1).hasMoreCard should be (true)
     }
 
+    "should be fair to both user after a lot of games" in {
+      val gameResults: Seq[GameResult] = (1 to 10000).map { _ =>
+        new Game(config).start()
+      }
 
+      val statistic = gameResults.foldLeft((0, 0, 0)) { (z, result) =>
+        result match {
+          case GameFinishedWithDrawGame =>
+            (z._1, z._2, z._3 + 1)
+          case GameFinishedWithWinner(winner, _, _) if winner.name == player0Name =>
+            (z._1 + 1, z._2, z._3)
+          case GameFinishedWithWinner(winner, _, _) if winner.name == player1Name =>
+            (z._1, z._2 + 1, z._3)
+        }
+      }
+      println(statistic)
+
+    }
   }
 }
