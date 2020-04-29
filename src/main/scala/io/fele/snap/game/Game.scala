89a67d8eea28ec0cb41ac9c0c3c6835756e74e26
feleio <lokling@gmail.com>
2020-04-29 20:24:09 +0100

    constructor for game to initialize game state

--- src/main/scala/io/fele/snap/game/Game.scala
+++ src/main/scala/io/fele/snap/game/Game.scala
@@ -1,10 +1,20 @@
 package io.fele.snap.game
 
-import io.fele.snap.model.Card
+import io.fele.snap.model.{Card, Deck}
+
+import scala.util.Random
 
 class Game(
   cfg: Config
 ) {
+  val config: Config = cfg
+  val allCards: List[Card] = (1 to config.numberOfDecks).flatMap(_ => Deck.cards).toList
+  val shuffled: List[Card] = Random.shuffle(allCards)
+  val players = Vector(
+    new Player(name = config.player0Name, cards = shuffled.take(shuffled.size/2)),
+    new Player(name = config.player1Name, cards = shuffled.drop(shuffled.size/2)),
+  )
+
   def start(): Unit = ???
 
   private def shouldMatch(pile: List[Card]): Boolean = ???
